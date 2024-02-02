852d1523daed8c38034363073e6aea4ab9c0e71b
N.I.K <85172286+masquerade28@users.noreply.github.com>
2024-02-02 19:40:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,66 @@
-function toggleMenu() 
-{
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
+// Selecting DOM elements using querySelector and getElementById
+const inputBox = document.querySelector('.input-box');
+const searchBtn = document.getElementById('searchBtn');
+const weather_img = document.querySelector('.weather-img');
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+const humidity = document.getElementById('humidity');
+const wind_speed = document.getElementById('wind-speed');
 
-    menu.classList.toggle("Open");
-    icon.classList.toggle("Open");
+const location_not_found = document.querySelector('.location-not-found');
+
+const weather_body = document.querySelector('.weather-body');
+
+// Async function to check weather using OpenWeatherMap API
+async function checkWeather(city){
+    // OpenWeatherMap API key and URL
+    const api_key = "d907d735dae8f7f6855e1b58b42a3181";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+    // Fetching weather data from the API
+    const weather_data = await fetch(`${url}`).then(response => response.json());
+
+    // Handling the case where the location is not found (HTTP 404 error)
+    if(weather_data.cod === `404`){
+        location_not_found.style.display = "flex";
+        weather_body.style.display = "none";
+        console.log("error");
+        return;
+    }
+
+    // Displaying the weather information and updating the UI
+    console.log("run");
+    location_not_found.style.display = "none";
+    weather_body.style.display = "flex";
+    temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
+    description.innerHTML = `${weather_data.weather[0].description}`;
+    humidity.innerHTML = `${weather_data.main.humidity}%`;
+    wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
+
+    // Updating the weather image based on the weather condition
+    switch(weather_data.weather[0].main){
+        case 'Clouds':
+            weather_img.src = "/assets/cloud.png";
+            break;
+        case 'Clear':
+            weather_img.src = "/assets/clear.png";
+            break;
+        case 'Rain':
+            weather_img.src = "/assets/rain.png";
+            break;
+        case 'Mist':
+            weather_img.src = "/assets/mist.png";
+            break;
+        case 'Snow':
+            weather_img.src = "/assets/snow.png";
+            break;
+    }
+
+    // Logging the weather data to the console
+    console.log(weather_data);
 }
+
+// Adding a click event listener to the search button
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(inputBox.value);
+});
