ca5f83285861ab5e3f4ad966d8a1d0c9fff5257a
masquerade28 <mystery.xelarvis@gmail.com>
2024-02-02 17:46:15 +0530

    Added Hamburger Menu

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() 
+{
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+
+    menu.classList.toggle("Open");
+    icon.classList.toggle("Open");
+}
